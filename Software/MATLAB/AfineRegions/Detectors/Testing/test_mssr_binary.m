51dde4f87ff5a2bab9f64183cf6df1781626da18
elboyran <e.ranguelova@esciencecenter.nl>
2015-05-15 12:58:41 +0200

    adding switch and case for the test cases

--- Software/MATLAB/AfineRegions/Detectors/Testing/test_mssr_binary.m
+++ Software/MATLAB/AfineRegions/Detectors/Testing/test_mssr_binary.m
@@ -13,51 +13,71 @@ visualize = false;
 if interactive 
     image_filename = input('Enter the test image filename: ','s');
 else
-    image_filename = fullfile('home','elena','eStep','LargeScaleImaging',...
-         'Data','Synthetic','Binary','binary_shapes.png');
-    image_filename = fullfile('home','elena','eStep','LargeScaleImaging',...
-        'Data','Synthetic','Binary','BasicSaliency.png');
-     image_filename = fullfile('home','elena','eStep','LargeScaleImaging',...
-         'Data','BinaryShapes','Selected','frog-15_mod.gif');
-    image_filename = fullfile('home','elena','eStep','LargeScaleImaging',...
-    'Data','BinaryShapes','Selected','frog-15_mod_new.gif');
-       image_filename = fullfile('home','elena','eStep','LargeScaleImaging',...
-           'Data','BinaryShapes','Selected','horseshoe-11.gif');      
-      image_filename = fullfile('home','elena','eStep','LargeScaleImaging',...
-          'Data','BinaryShapes','Selected','horseshoe-19.gif'); 
-       image_filename = fullfile('home','elena','eStep','LargeScaleImaging',...
-           'Data','BinaryShapes','Selected','device9-15_mod.gif');
-     image_filename = fullfile('home','elena','eStep','LargeScaleImaging',...
-         'Data','BinaryShapes','Selected','butterfly-2.gif');
-     image_filename = fullfile('home','elena','eStep','LargeScaleImaging',...
-         'Data','BinaryShapes','Selected','butterfly-1.gif');
- end
+    test_image = input('Enter test case: [binary_test|binary_shapes|basic_saliency|frog|device|butterfly|horseshoe]: ','s');
+    switch lower(test_image)
+        case 'binary_test'
+            image_filename{1} = fullfile('home','elena','eStep','LargeScaleImaging',...
+            'Data','Synthetic','Binary','TestBinarySaliency.png');
+        case 'binary_shapes'
+            image_filename{1} = fullfile('home','elena','eStep','LargeScaleImaging',...
+            'Data','Synthetic','Binary','binary_shapes.png');
+        case 'basic_saliency'
+            image_filename{1} = fullfile('home','elena','eStep','LargeScaleImaging',...
+            'Data','Synthetic','Binary','BasicSaliency.png');
+        case 'frog'
+            image_filename{1} = fullfile('home','elena','eStep','LargeScaleImaging',...
+            'Data','BinaryShapes','Selected','frog-15_mod.gif');
+            image_filename{2} = fullfile('home','elena','eStep','LargeScaleImaging',...
+            'Data','BinaryShapes','Selected','frog-15_mod_new.gif');
+        case 'device'
+            image_filename{1} = fullfile('home','elena','eStep','LargeScaleImaging',...
+           'Data','BinaryShapes','Selected','device9-15_mod.gif');   
+        case 'butterfly'
+            image_filename{1} = fullfile('home','elena','eStep','LargeScaleImaging',...
+            'Data','BinaryShapes','Selected','butterfly-2.gif');
+            image_filename{2} = fullfile('home','elena','eStep','LargeScaleImaging',...
+            'Data','BinaryShapes','Selected','butterfly-1.gif'); 
+        case 'horseshoe'
+            image_filename{1} = fullfile('home','elena','eStep','LargeScaleImaging',...
+            'Data','BinaryShapes','Selected','horseshoe-11.gif');      
+            image_filename{2} = fullfile('home','elena','eStep','LargeScaleImaging',...
+            'Data','BinaryShapes','Selected','horseshoe-19.gif'); 
+    end
 
-%% load the image 
-image_data = logical(imread(image_filename));
 
-
-%% run the MSSR detector on the test image
-if interactive
-    saliency_types(1) = input('Detect "holes"? [0/1]: ');
-    saliency_types(2) = input('Detect "islands"? [0/1]: ');
-    saliency_types(3) = input('Detect "indentations"? [0/1]: ');
-    saliency_types(4) = input('Detect "protrusions"? [0/1]: ');
-    SE_size_factor = input('Enter the Structuring Element size factor: ');
-    Area_factor = input('Enter the Connected Component size factor: ');
-else
-    saliency_types = [1 1 1 1];
-    SE_size_factor = 0.02;
-    Area_factor = 0.03;
 end
 
-tic
-[saliency_masks] = mssr_binary(image_data, SE_size_factor,Area_factor,...
-    saliency_types, visualize);
-toc
-%% visualize
-% visualize original image
-f = figure; subplot(1, 2, 1); visualize_mssr_binary(image_data); axis on, grid on;
-% visualize regions
-figure(f);subplot(1, 2, 2);visualize_mssr_binary(image_data, saliency_masks,...
-    [1 1 1 1], Area_factor, SE_size_factor); axis on; grid on;
+%% find out the number of test files
+len = length(image_filename);
+
+%% loop over all test images
+for i = 1:len
+    %% load the image
+    image_data = logical(imread(image_filename{i}));
+
+
+    %% run the MSSR detector on the test image
+    if interactive
+        saliency_types(1) = input('Detect "holes"? [0/1]: ');
+        saliency_types(2) = input('Detect "islands"? [0/1]: ');
+        saliency_types(3) = input('Detect "indentations"? [0/1]: ');
+        saliency_types(4) = input('Detect "protrusions"? [0/1]: ');
+        SE_size_factor = input('Enter the Structuring Element size factor: ');
+        Area_factor = input('Enter the Connected Component size factor: ');
+    else
+        saliency_types = [1 1 1 1];
+        SE_size_factor = 0.02;
+        Area_factor = 0.03;
+    end
+
+    tic
+    [saliency_masks] = mssr_binary(image_data, SE_size_factor,Area_factor,...
+        saliency_types, visualize);
+    toc
+    %% visualize
+    % visualize original image
+    f = figure; subplot(1, 2, 1); visualize_mssr_binary(image_data); axis on, grid on;
+    % visualize regions
+    figure(f);subplot(1, 2, 2);visualize_mssr_binary(image_data, saliency_masks,...
+        [1 1 1 1], Area_factor, SE_size_factor); axis on; grid on;
+end
