cb18c43432697b427c42c0dd514dea7334474203
elboyran <e.ranguelova@esciencecenter.nl>
2015-06-03 16:11:41 +0200

    added processing of all test cases in a directory

--- Software/MATLAB/AfineRegions/Detectors/Testing/test_smssr_demo.m
+++ Software/MATLAB/AfineRegions/Detectors/Testing/test_smssr_demo.m
@@ -56,125 +56,129 @@ end
 data_test_path = fullfile(domain_path, test_case_name);
 results_domain_path = fullfile(domain_results_path,test_case_name); 
 
-switch lower(test_case_name)
-    case 'leatherback'
-        image = '0517071a'
-        image_filename{1} = fullfile(data_test_path,[image '.pgm'])
-        if save_flag
-            features_filename{1} = fullfile(results_domain_path,[image '.smssr'])
-            regions_filename{1} = fullfile(results_domain_path, [image '_smartregions.mat'])
-        end
+%switch lower(test_case_name)
+    %case 'leatherback'
+        %image = '0517071a'
+        %image_filename{1} = fullfile(data_test_path,[image '.pgm']);
+        %if save_flag
+        %    features_filename{1} = fullfile(results_domain_path,[image '.smssr']);
+        %    regions_filename{1} = fullfile(results_domain_path, [image '_smartregions.mat']);
+        %end
         
- end
- mask_filename =[];
+ %end
+images_list = dir(data_test_path);
+ 
+mask_filename =[];
 
 disp('**************************** Demo SMSSR detector *****************');
 %% find out the number of test files
-len = length(image_filename);
+len = length(images_list);
 
 %% loop over all test images
 for i = 1:len
-    %% load the image & convertto gray-scale if  color
-    image_data = imread(image_filename{i});
-    if ndims(image_data) > 2
-        image_data = rgb2gray(image_data);
-    end
+    if not images_list(i).isdir
+      image_filename = fullfile(data_test_path, images_list(i).name);
+      %% load the image & convertto gray-scale if  color
+      image_data = imread(image_filename);
+      if ndims(image_data) > 2
+	  image_data = rgb2gray(image_data);
+      end
     
-    %% load the mask, if any
-    ROI = [];
-    if ~isempty(mask_filename)
-        s = load(ROI_mask_fname);
-        s_cell = struct2cell(s);
-        for k = 1:size(s_cell)
-            field = s_cell{k};
-            if islogical(field)
-                ROI = field;
-            end
-        end
-        if isempty(ROI)
-           error('ROI_mask_fname does not contain binary mask!');
-        end
-    end
+      %% load the mask, if any
+      ROI = [];
+      if ~isempty(mask_filename)
+	  s = load(ROI_mask_fname);
+	  s_cell = struct2cell(s);
+	  for k = 1:size(s_cell)
+	      field = s_cell{k};
+	      if islogical(field)
+		  ROI = field;
+	      end
+	  end
+	  if isempty(ROI)
+	    error('ROI_mask_fname does not contain binary mask!');
+	  end
+      end
 
-    %% run the SMSSR detector
+      %% run the SMSSR detector
+      
+      tic;
+      
+      if interactive
+	  preproc_types(1) = input('Smooth? [0/1]: ');
+	  preproc_types(2) = input('Histogram equialize? [0/1]: ');
+	  SE_size_factor_preproc = input('Enter the Structuring Element size factor (preprocessing): ');
+	  saliency_types(1) = input('Detect "holes"? [0/1]: ');
+	  saliency_types(2) = input('Detect "islands"? [0/1]: ');
+	  saliency_types(3) = input('Detect "indentations"? [0/1]: ');
+	  saliency_types(4) = input('Detect "protrusions"? [0/1]: ');
+	  SE_size_factor = input('Enter the Structuring Element size factor: ');
+	  Area_factor = input('Enter the Connected Component size factor (processing): ');
+	  num_levels = input('Enter the number of gray-levels: ');
+	  thresh = input('Enter the region threshold: ');
+	  
+      else
+	  preproc_types = [0 1];
+	  saliency_types = [1 1 1 1];
+	  SE_size_factor = 0.02;
+	  SE_size_factor_preproc = 0.002;
+	  Area_factor = 0.03;
+	  num_levels = 20;
+	  thersh = 0.75;
+      end
+      
+      
+      disp('Test case: ');disp(test_case_name);
+      disp('SMSSR');
+	
+      region_params = [SE_size_factor Area_factor];
+      execution_params = [verbose visualize_major visualize_minor];
+      image_data = smssr_preproc(image_data, preproc_types);
+      [num_smartregions, features, saliency_masks] = smssr(image_data, ROI, ...
+	  num_levels, saliency_types, region_params, execution_params);
+      toc
+      % save the features
+      disp('Saving ...');
+      
+      smssr_save(features_filename{i}, regions_filename{i}, num_smartregions, features, saliency_masks);
+      
+      
+      %% visualize
+  %   if visualize
+  %       f1 = figure; set(f1,'WindowStyle','docked');visualize_mssr(image_data);
+  %       f2 = figure; set(f2,'WindowStyle','docked');visualize_mssr(image_data, saliency_masks, saliency_types, region_params);
+  %       f3 = figure; set(f3,'WindowStyle','docked');visualize_mssr(image_data, saliency_masks, [1 0 0 0], region_params);
+  %       f4 = figure; set(f4,'WindowStyle','docked');visualize_mssr(image_data, saliency_masks, [0 1 0 0], region_params);
+  %       f5 = figure; set(f5,'WindowStyle','docked');visualize_mssr(image_data, saliency_masks, [0 0 1 0], region_params);
+  %       f6 = figure; set(f6,'WindowStyle','docked');visualize_mssr(image_data, saliency_masks, [0 0 0 1], region_params);
+	
+  %   end
+    if vis_flag
+	disp(' Displaying... ');
+	
+	type = 1; % distinguish region's types
     
-    tic;
+	% display the saved regions
+	%[num_smartregions, features, saliency_masks] = smssr_open(features_filename{i}, regions_filename{1}, type);
+      
+	list_smartregions = [];     % display all regions
     
-    if interactive
-        preproc_types(1) = input('Smooth? [0/1]: ');
-        preproc_types(2) = input('Histogram equialize? [0/1]: ');
-        SE_size_factor_preproc = input('Enter the Structuring Element size factor (preprocessing): ');
-        saliency_types(1) = input('Detect "holes"? [0/1]: ');
-        saliency_types(2) = input('Detect "islands"? [0/1]: ');
-        saliency_types(3) = input('Detect "indentations"? [0/1]: ');
-        saliency_types(4) = input('Detect "protrusions"? [0/1]: ');
-        SE_size_factor = input('Enter the Structuring Element size factor: ');
-        Area_factor = input('Enter the Connected Component size factor (processing): ');
-        num_levels = input('Enter the number of gray-levels: ');
-        thresh = input('Enter the region threshold: ');
-        
-    else
-        preproc_types = [0 1];
-        saliency_types = [1 1 1 1];
-        SE_size_factor = 0.02;
-        SE_size_factor_preproc = 0.002;
-        Area_factor = 0.03;
-        num_levels = 20;
-        thersh = 0.75;
+	scaling = 1;  % no scaling
+	line_width = 2; % thickness of the line
+	labels = 0; % no region's labels
+    
+	col_ellipse = [];
+	col_label = [];
+      
+	original = 0; % no original region's outline
+      
+	display_smart_regions(image_filename{i}, features_filename{i}, mask_filename, ...
+		      regions_filename{i},...  
+		      type, list_smartregions, scaling, labels, col_ellipse, ...
+		      line_width, col_label, original);
+	title('SMSSR');
+      end
     end
-    
-    
-    disp('Test case: ');disp(test_case_name);
-    disp('SMSSR');
-      
-    region_params = [SE_size_factor Area_factor];
-    execution_params = [verbose visualize_major visualize_minor];
-    image_data = smssr_preproc(image_data, preproc_types);
-    [num_smartregions, features, saliency_masks] = smssr(image_data, ROI, ...
-        num_levels, saliency_types, region_params, execution_params);
-    toc
-    % save the features
-    disp('Saving ...');
-    
-    smssr_save(features_filename{i}, regions_filename{i}, num_smartregions, features, saliency_masks);
-    
-    
-    %% visualize
- %   if visualize
- %       f1 = figure; set(f1,'WindowStyle','docked');visualize_mssr(image_data);
- %       f2 = figure; set(f2,'WindowStyle','docked');visualize_mssr(image_data, saliency_masks, saliency_types, region_params);
- %       f3 = figure; set(f3,'WindowStyle','docked');visualize_mssr(image_data, saliency_masks, [1 0 0 0], region_params);
- %       f4 = figure; set(f4,'WindowStyle','docked');visualize_mssr(image_data, saliency_masks, [0 1 0 0], region_params);
- %       f5 = figure; set(f5,'WindowStyle','docked');visualize_mssr(image_data, saliency_masks, [0 0 1 0], region_params);
- %       f6 = figure; set(f6,'WindowStyle','docked');visualize_mssr(image_data, saliency_masks, [0 0 0 1], region_params);
-      
- %   end
-  if vis_flag
-      disp(' Displaying... ');
-      
-      type = 1; % distinguish region's types
-   
-      % display the saved regions
-      %[num_smartregions, features, saliency_masks] = smssr_open(features_filename{i}, regions_filename{1}, type);
-    
-      list_smartregions = [];     % display all regions
-   
-      scaling = 1;  % no scaling
-      line_width = 2; % thickness of the line
-      labels = 0; % no region's labels
-   
-      col_ellipse = [];
-      col_label = [];
-    
-      original = 0; % no original region's outline
-    
-      display_smart_regions(image_filename{i}, features_filename{i}, mask_filename, ...
-		    regions_filename{i},...  
-		    type, list_smartregions, scaling, labels, col_ellipse, ...
-		    line_width, col_label, original);
-      title('SMSSR');
-    end
-    
      
 end
 disp('--------------- The End ---------------------------------');
