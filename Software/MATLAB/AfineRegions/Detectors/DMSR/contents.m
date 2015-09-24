9ff29f1c5da2236eabc3d529c54d5dd0fa4cdd19
elboyran <e.ranguelova@esciencecenter.nl>
2015-09-24 17:57:57 +0200

    new directory for the new DMSR Detector

---
+++ Software/MATLAB/AfineRegions/Detectors/DMSR/contents.m
@@ -0,0 +1,33 @@
+% contents.m- contents of directory ...\DMSR
+%
+% topic: Data-driven Morphologically-based Salient Regions (DMSR) detection 
+% author: Elena Ranguelova, NLeSc
+% date: September 2015
+%
+%**************************************************************************
+% functions
+%**************************************************************************
+%--------------------------------------------------------------------------
+% main
+%--------------------------------------------------------------------------
+
+%--------------------------------------------------------------------------
+% secondary
+%--------------------------------------------------------------------------
+
+% dmsr_save- function to save the results from the DMSR detector
+% dmsr_open- function to open the saved results from the DMSR detector
+%**************************************************************************
+% scripts
+%**************************************************************************
+
+%**************************************************************************
+% misc (in the MATLAB path)
+%**************************************************************************
+% cl - clear the workspace and the screen 
+%
+% MyColormaps.mat - custom colour map (mycmap) for displaying the 
+%                                                accumulative saliency maps
+% list_col_ellipse.mat- file containing 343 different colours to display
+%                       features as ellipses
+% freezeColors.m-  lock colors of an image to current colors
--- Software/MATLAB/AfineRegions/Detectors/contents.m
+++ Software/MATLAB/AfineRegions/Detectors/contents.m
@@ -2,9 +2,11 @@
 %
 % topic: Salient regions detection
 % author: Elena Ranguelova, NLeSc
-% date: April 2015
+% date: September 2015
 %
 %**************************************************************************
+% DMSR directory- contains the code for the DMSR detector
+% SMSSR directory- contains the code for the SMSSR detector
 % MSSR directory- contains the code for the MSSR detector
 % MSER directory- contains the code for the MSER detector
 % Testing directory- contains the scripts for repeatability testing
