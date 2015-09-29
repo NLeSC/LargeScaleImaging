c6207bfd8b3f9161f4b4f2ea6e4b1be1e53aa215
elboyran <e.ranguelova@esciencecenter.nl>
2015-09-29 17:34:51 +0200

    fuller version of test_mser_LMwood.m

--- Software/MATLAB/WoodAnatomy/Testing/test_mser_LMwood.m
+++ Software/MATLAB/WoodAnatomy/Testing/test_mser_LMwood.m
@@ -6,28 +6,52 @@
 % last modification date: 
 % modification details: 
 
-% data
+%% paths and filenames
 data_path = '/home/elena/eStep/LargeScaleImaging/Data/Scientific/WoodAnatomy/LM pictures wood/PNG';
-%image_filename = 'Ocinotis gracilis02.png';
-%image_filename = 'Argania spinosa1.png';
-image_filename = 'Carini decr01.png';
+results_path ='/home/elena/eStep/LargeScaleImaging/Results/Scientific/WoodAnatomy/LM pictures wood';
 
+%test_case = input('Enter test case: [anodendron|argania|carini|crater|dregia|gymnema|napol|ocinotis|periploca]: ','s');
+test_case = 'anodendron';
+switch lower(test_case)
+    case 'anodendron'
+        image_filename = 'Anodendron rubescens01.png';
+    case 'argania'
+        image_filename = 'Argania spinosa1.png';
+    case 'carini'
+        image_filename = 'Carini decr01.png';
+    case 'crater'
+        image_filename = 'Crater letest8603_01.png';
+    case 'dregia'
+        image_filename = 'Dregia volubilis04.png';
+    case 'gymnema'
+        image_filename = 'Gymnema tingens04.png';
+    case 'napol'
+        image_filename = 'Napol vog 41120_01.png';
+    case 'ocinotis'
+        image_filename = 'Ocinotis gracilis02.png';
+    case 'periploca'
+        image_filename = 'Periploca laevigata02.png';
+end  
 
-filename = fullfile(data_path, image_filename);
+data_filename = fullfile(data_path, image_filename);
+result_filename = fullfile(results_path, [test_case '.mat']);
 
-% load
-image_data_orig = imread(filename);
+%% load
+image_data_orig = imread(data_filename);
 
 if ~ismatrix(image_data_orig)
     image_data_2D = rgb2gray(image_data_orig);
 end
 
-% extract
+%% extract
 tic
 MSERregions = detectMSERFeatures(image_data_2D);
 toc
 
-% visualize
+%% save
+save(result_filename,'MSERregions');
+
+%% visualize
 figure; imshow(image_data_2D); hold on; 
 plot(MSERregions, 'showPixelList', true, 'showEllipses', false);
 title(['MSER exact regions for ' num2str(image_filename)]);
