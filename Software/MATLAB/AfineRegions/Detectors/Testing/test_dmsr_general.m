2013f8ca18f077802fe3bd39228b8788ed3173ec
elboyran <e.ranguelova@esciencecenter.nl>
2015-10-05 18:46:38 +0200

    testing DMSR metric

--- Software/MATLAB/AfineRegions/Detectors/Testing/test_dmsr_general.m
+++ Software/MATLAB/AfineRegions/Detectors/Testing/test_dmsr_general.m
@@ -13,7 +13,7 @@ visualize_major = false;
 visualize_minor = false;
 lisa = false;
 
-batch_structural = true;
+batch_structural = false;
 
 save_flag = 1;
 vis_flag = 1;
@@ -37,7 +37,7 @@ else
     if batch_structural
         test_images = {'boat', 'bikes', 'graffiti', 'leuven' ,'ubc'};
     else
-        test_images = {'boat'};
+        test_images = {'graffiti'};
     end
     mask_filename =[];
     
@@ -48,13 +48,13 @@ for test_image = test_images
     [image_filenames, features_filenames, regions_filenames] = ...
         get_test_filenames(test_image, 'dmsr', data_path, results_path);
     
-    disp('**************************** Testing SMSSR detector *****************');
+    disp('**************************** Testing DMSR detector *****************');
     %% find out the number of test files
     len = length(image_filenames);
     
     %% loop over all test images
     for i = 1:len
-        %for i = 2
+        %for i = 1
         %% load the image & convertto gray-scale if  color
         image_data = imread(char(image_filenames{i}));
         if ndims(image_data) > 2
@@ -96,7 +96,7 @@ for test_image = test_images
 %         else
 %             preproc_types = [0 0];
 %             saliency_types = [1 1 0 0];
-%             SE_size_factor = 0.02;
+             SE_size_factor = 0.02;
 %             SE_size_factor_preproc = 0.002;
 %             Area_factor = 0.03;
              num_levels = 255;
@@ -105,6 +105,7 @@ for test_image = test_images
 %             thresh_type = 's';
 %             %saliency_thresh = [0.05 0.15 0.25 0.5 0.75];
 %             saliency_thresh = 0.6;
+             pref_offset = 20;
 %         end
         
         load('MyColormaps','mycmap'); 
@@ -112,23 +113,81 @@ for test_image = test_images
         tic;
         disp('Test case: ');disp(test_image);
         disp('DMSR');
-        [r,c] =  size(image_data);
-        binary_masks = zeros(r,c, num_levels);
+        [nrows,ncols] =  size(image_data);
+        Area = nrows*ncols;
+        lambda = 2* fix(SE_size_factor*sqrt(Area/pi));
+        binary_masks = zeros(nrows,ncols, num_levels);
+        
+        % otsu thresholding
+        otsu = fix(num_levels*graythresh(image_data));
+        binary_otsu = image_data >= otsu;
+        
+        
         for level = 1:num_levels
             binary = image_data >= level;
-            binary_masks(:,:,level) = binary;
+            binary_filt = bwareaopen(binary, lambda, 8);
+            binary_filt = 1- bwareaopen(1- binary_filt, lambda, 8);
+            binary_masks(:,:,level) = binary_filt;
+        end
+        
+        for l = 1 :num_levels
+              CC{l} = bwconncomp(binary_masks(:,:,l),8);
+              ncc = CC{l}.NumObjects;
+              if ncc > 20
+                num_cc(l) = ncc;
+              else
+                num_cc(l) =NaN;
+              end;
+             % RP{l} = regionprops( CC , 'Area');
         end
        
-        for l = 2:255
-        d =  xor( binary_masks(:,:,l-1), binary_masks(:,:,l) ) ;
-        m(l) = sum(d(:));
+        offset = 20; 
+        for l = offset+1 : num_levels - offset;
+            d = 0;
+            for o = 1:offset
+              diff_f = abs(num_cc(l) - num_cc(l+o));
+              diff_b = abs(num_cc(l) - num_cc(l-o));
+              d = [d (diff_f + diff_b)/2];
+            end              
+              diff(l) = sum(d(:));
         end
-       % figure;plot(m); grid on;
+                
+%         [max_l, max_ind] = max(diff(otsu - offset:otsu + offset));
+%         thresh = max_ind + (otsu - (offset + 1));
         
-        offset = 50;
-        [max_l, max_ind] = max(m(offset:num_levels - offset));
-        thresh = max_ind + offset;
-        figure;imshow(binary_masks(:,:,thresh)); grid on;
+%         [min_l, min_ind] = min(diff(otsu - offset:otsu + offset));
+%         thresh = min_ind + (otsu - (offset+1));
+        
+         cropped_diff = diff(pref_offset:255 - pref_offset); 
+         [val, ind] = max(cropped_diff(:));
+         thresh = ind + (pref_offset - 1);
+       
+        
+%           disp('Ratio between max and mean of the differences: ');
+%           disp(max_l/mean(cropped_diff(:)));
+%           disp('Standart deviation of the differences');
+%           disp(std(cropped_diff(:)));
+          
+        [counts, centers]= hist(double(image_data(:)), num_levels); 
+        
+        figure('Position',get(0,'ScreenSize'))
+        subplot(221); bar(centers, counts); title('Gray-level histogram');
+                hold on; line('XData',[otsu otsu], ...
+                    'YData', [0 max(counts)], 'Color', 'r'); hold off;axis on;grid on;
+        subplot(222);imshow(binary_otsu); axis on;grid on;
+        title(['Image thresholded at Otsu s level ' num2str(otsu)]);
+        
+        subplot(223); plot(diff, 'k'); title('Differences num_regions.')
+        hold on; line('XData',[thresh thresh], ...
+                    'YData', [0 max(diff)], 'Color', 'r'); 
+                line('XData',[pref_offset pref_offset], ...
+                    'YData', [0 max(diff)], 'Color', 'g'); 
+                line('XData',[255- pref_offset 255 - pref_offset], ...
+                    'YData', [0 max(diff)], 'Color', 'g');
+                hold off;axis on; grid on;
+                                                
+        subplot(224); imshow(binary_masks(:,:,thresh)); axis on;grid on;
+        title(['Image thresholded at diff threshold ' num2str(thresh)]);
     
 %         acc_mask = integral_masks(binary_masks);
 %         
@@ -176,7 +235,7 @@ for test_image = test_images
 %                 list_smartregions, step_list_regions, scaling, labels, col_ellipse, ...
 %                 line_width, col_label, original);
 %         end
-        
+        clear binary binary_otsu binary_masks RP CC image_data
     end
 end
 disp('--------------- The End ---------------------------------');
