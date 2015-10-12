7d21f451c857035514210d0cdf3a401f7425dd48
elboyran <e.ranguelova@esciencecenter.nl>
2015-10-12 21:02:40 +0200

    added batch procesisng

--- Software/MATLAB/AfineRegions/Detectors/Testing/repeatability_demo/repeatability_demo.m
+++ Software/MATLAB/AfineRegions/Detectors/Testing/repeatability_demo/repeatability_demo.m
@@ -1,55 +1,66 @@
 function repeatability_demo
 
-det_suffix={'mser';'mssr'; 'smssr';'dmsr'};
-
+det_suffix={'mser';'mssr'; 'dmsr'};
+batch_structural = true;
 lisa = false;
 %% image filename
-if ispc 
+if ispc
     starting_path = fullfile('C:','Projects');
 elseif lisa
-     starting_path = fullfile(filesep, 'home','elenar');
+    starting_path = fullfile(filesep, 'home','elenar');
 else
     starting_path = fullfile(filesep,'home','elena');
 end
 project_path = fullfile(starting_path, 'eStep','LargeScaleImaging');
 data_path = fullfile(project_path, 'Data', 'AffineRegions');
 results_path = fullfile(project_path, 'Results', 'AffineRegions');
-test_case = input('Enter the test case : [bark|bikes|boat|graffiti|leuven|trees|ubc|wall]: ','s');
-
-feature_filename = fullfile(results_path,test_case,test_case);
-image_filename = fullfile(data_path,test_case,'img');
-%% repeatabiliy figures
-f1 = figure; f2 =  figure;
-figure(f1);clf;
-grid on;
-ylabel('repeatebility %')
-xlabel('transformation strength');
-hold on;
-figure(f2);clf;
-grid on;
-ylabel('nb of correspondences')
-xlabel('transformation strength');
-hold on;
-
-mark=['-gs';'-bv';'-rp';'-mh'];
-for d=1:4
-    seqrepeat=[];
-    seqcorresp=[];
-    for i=2:6
-        file1=sprintf('%s1.%s',feature_filename,det_suffix{d});
-        file2=sprintf( '%s%d.%s',feature_filename, i,det_suffix{d});
-        Hom=fullfile(data_path, test_case, sprintf('H1to%dp',i));
-        imf1=[image_filename '1.ppm'];
-        imf2=sprintf('%s%d.ppm',image_filename,i);
-        [erro,repeat,corresp, match_score,matches, twi]=repeatability(file1,file2,Hom,imf1,imf2, 1);
-        seqrepeat=[seqrepeat repeat(4)];
-        seqcorresp=[seqcorresp corresp(4)];
-    end
-    figure(f1);  plot([20 30 40 50 60],seqrepeat,mark(d,:));
-    figure(f2);  plot([20 30 40 50 60],seqcorresp,mark(d,:));
+%test_case = input('Enter the test case : [bark|bikes|boat|graffiti|leuven|trees|ubc|wall]: ','s');
+if batch_structural
+    test_cases = {'boat', 'bikes', 'graffiti', 'leuven'};
 end
 
-figure(f1);legend(det_suffix{1},det_suffix{2},det_suffix{3}, det_suffix{4});
-axis([10 70 0 100]);
-figure(f2);legend(det_suffix{1},det_suffix{2},det_suffix{3}, det_suffix{4});
+for test_case_cell = test_cases
+    test_case = char(test_case_cell);
+    disp(test_case);
+    feature_filename = fullfile(results_path,test_case,test_case);
+    image_filename = fullfile(data_path,test_case,'img');
+    %% repeatabiliy figures
+    f1 = figure; f2 =  figure;
+    figure(f1);clf;
+    grid on;
+    ylabel('repeatability %')
+    xlabel('transformation strength');
+    title(test_case);
+    hold on;
+    figure(f2);clf;
+    grid on;
+    ylabel('nb of correspondences')
+    xlabel('transformation strength');
+    title(test_case);
+    hold on;
+    
+    mark=['-gs';'-bv';'-rp'];
+    for d=1:3
+        seqrepeat=[];
+        seqcorresp=[];
+        for i=2:6
+            file1=sprintf('%s1.%s',feature_filename,det_suffix{d});
+            file2=sprintf( '%s%d.%s',feature_filename, i,det_suffix{d});
+            Hom=fullfile(data_path, test_case, sprintf('H1to%dp',i));
+            imf1=[image_filename '1.ppm'];
+            imf2=sprintf('%s%d.ppm',image_filename,i);
+            [erro,repeat,corresp, match_score,matches, twi]=repeatability(file1,file2,Hom,imf1,imf2, 1);
+            seqrepeat=[seqrepeat repeat(4)];
+            seqcorresp=[seqcorresp corresp(4)];
+        end
+        figure(f1);  plot([20 30 40 50 60],seqrepeat,mark(d,:));
+        figure(f2);  plot([20 30 40 50 60],seqcorresp,mark(d,:));
+    end
+    
+    figure(f1);legend(det_suffix{1},det_suffix{2},det_suffix{3});
+    axis([10 70 0 100]);
+    figure(f2);legend(det_suffix{1},det_suffix{2},det_suffix{3});
+    pause(1);
 
+    disp('--------------------------------');
+end
