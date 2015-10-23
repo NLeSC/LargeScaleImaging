f3728106c2e0e179196080610d883edf42eef5d4
elboyran <e.ranguelova@esciencecenter.nl>
2015-10-23 19:32:37 +0200

    progress region properties of wood images

--- Software/MATLAB/WoodAnatomy/Testing/test_regionprops_LMwood.m
+++ Software/MATLAB/WoodAnatomy/Testing/test_regionprops_LMwood.m
@@ -2,7 +2,7 @@
 %**************************************************************************
 %
 % author: Elena Ranguelova, NLeSc
-% date created: 19 October 2015
+% date created: 23 October 2015
 % last modification date: 
 % modification details:
 
@@ -18,25 +18,130 @@ data_path = '/home/elena/eStep/LargeScaleImaging/Data/Scientific/WoodAnatomy/LM 
 results_path ='/home/elena/eStep/LargeScaleImaging/Results/Scientific/WoodAnatomy/LM pictures wood';
 detector  = 'DMSR';
 
-for test_case = {'Argania','Brazzeia_c', 'Brazzeia_s', 'Chrys', 'Citronella',...
-        'Desmo', 'Gluema', 'Rhaptop', 'Stem'}
+for test_case = {'Argania' ,'Brazzeia_c'} %, 'Brazzeia_s', 'Chrys', 'Citronella',...
+        %'Desmo', 'Gluema', 'Rhaptop', 'Stem'}
     disp(['Processing species: ' test_case]);
     
     %% get the filenames
     [image_filenames, features_filenames, regions_filenames] = ...
         get_wood_test_filenames(test_case, detector, data_path, results_path);
     
-    %% load the detected DMSR regions
+    
     num_images = length(image_filenames);
     
     for i = 1:num_images
+    %for i = 1
         regions_filename = char(regions_filenames{i});
         
         if verbose
             disp(regions_filename);
         end
+        
+        %% load the detected DMSR regions
         load(regions_filename);
-        figure; imshow
+        
+        %% visualize
+        % how many types of regions?
+        sal_types = size(saliency_masks,3);
+        if visualize
+            figure; 
+            switch sal_types
+                case 1
+                    x = 1; y = 1;
+                case 2
+                    x = 1; y = 2;
+                case {3, 4}
+                    x=2; y= 2;
+            end
+            j = 0;
+            if sal_types > 0
+                j = j+ 1;
+                subplot(x,y,j);
+                L = bwlabel(saliency_masks(:,:,j));
+                imshow(label2rgb(L));title('Salient regions of same type labelled');
+            end
+            if sal_types > 1
+                j = j+ 1;
+                L = bwlabel(saliency_masks(:,:,j));
+                subplot(x,y,j);
+                imshow(label2rgb(L));title('Salient regions  of same type labelled');
+            end
+            if sal_types > 2
+                j = j+ 1;
+                L = bwlabel(saliency_masks(:,:,j));
+                subplot(x,y,j);
+                imshow(label2rgb(L));title('Salient regions of same type labelled');
+            end
+            if sal_types > 3
+                j = j+ 1;
+                L = bwlabel(saliency_masks(:,:,j));
+                subplot(x,y,j);
+                mshow(label2rgb(L)); title('Salient regions of same type labelled');
+            end
+        end
+        
+        %% compute region properties of the salient regions
+        j = 0;
+        if sal_types > 0
+            j = j+ 1;
+            BW = saliency_masks(:,:,j);
+            props = regionprops(BW, 'Area', 'Centroid','ConvexArea', ...
+                'Eccentricity', 'EquivDiameter', 'MinorAxisLength',...
+                'MajorAxisLength', 'Orientation');
+            AllAreas = props.Area;  
+            AllConvexAreas = props.ConvexArea;           
+            AllCentroids = props.Centroid;
+            AllDiameters = props.EquivDiameter;
+            AllOrientaitons = props.Orientation;
+            AllMinorAxes = props.MinorAxisLength; 
+            AllMajorAxes = props.MajorAxisLength; 
+            AllEccentricities = props.Eccentricity;
+        end
+        if sal_types > 1
+            j = j+ 1;
+            BW = saliency_masks(:,:,j);
+            props = regionprops(BW, 'Area', 'Centroid','ConvexArea', ...
+                'Eccentricity', 'EquivDiameter', 'MinorAxisLength',...
+                'MajorAxisLength', 'Orientation');
+            AllAreas = [AllAreas props.Area];  
+            AllConvexAreas = [AllConvexAreas props.ConvexArea];           
+            AllCentroids = [AllCentroids props.Centroid];
+            AllDiameters = [AllDiameters props.EquivDiameter];
+            AllOrientaitons = [AllOrientaitons props.Orientation];
+            AllMinorAxes = [AllMinorAxes  props.MinorAxisLength]; 
+            AllMajorAxes = [AllMajorAxes props.MajorAxisLength]; 
+            AllEccentricities = [AllEccentricities props.Eccentricity];
+        end
+        if sal_types > 2
+            j = j+ 1;
+            BW = saliency_masks(:,:,j);
+            props = regionprops(BW, 'Area', 'Centroid','ConvexArea', ...
+                'Eccentricity', 'EquivDiameter', 'MinorAxisLength',...
+                'MajorAxisLength', 'Orientation');
+            AllAreas = [AllAreas props.Area];  
+            AllConvexAreas = [AllConvexAreas props.ConvexArea];           
+            AllCentroids = [AllCentroids props.Centroid];
+            AllDiameters = [AllDiameters props.EquivDiameter];
+            AllOrientaitons = [AllOrientaitons props.Orientation];
+            AllMinorAxes = [AllMinorAxes  props.MinorAxisLength]; 
+            AllMajorAxes = [AllMajorAxes props.MajorAxisLength]; 
+            AllEccentricities = [AllEccentricities props.Eccentricity];           
+        end
+        if sal_types > 3
+            j = j+ 1;
+            BW = saliency_masks(:,:,j);
+            props = regionprops(BW, 'Area', 'Centroid','ConvexArea', ...
+                'Eccentricity', 'EquivDiameter', 'MinorAxisLength',...
+                'MajorAxisLength', 'Orientation');
+            AllAreas = [AllAreas props.Area];  
+            AllConvexAreas = [AllConvexAreas props.ConvexArea];           
+            AllCentroids = [AllCentroids props.Centroid];
+            AllDiameters = [AllDiameters props.EquivDiameter];
+            AllOrientaitons = [AllOrientaitons props.Orientation];
+            AllMinorAxes = [AllMinorAxes  props.MinorAxisLength]; 
+            AllMajorAxes = [AllMajorAxes props.MajorAxisLength]; 
+            AllEccentricities = [AllEccentricities props.Eccentricity];
+        end
         clear saliency_masks
     end
     
