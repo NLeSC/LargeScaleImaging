1802750a0256ebb6b40b25e44354b0c2e6c2c56d
elboyran <e.ranguelova@esciencecenter.nl>
2015-11-03 15:06:42 +0100

    added some generic tools for Wood classification

---
+++ Software/MATLAB/WoodAnatomy/Tools/contents.m
@@ -0,0 +1,16 @@
+% contents.m- contents of directory ...\WoodAnatomy\Tools
+%
+% topic: Wood anatomy classification tools
+% author: Elena Ranguelova, NLeSc
+% date: November 2015
+%
+%**************************************************************************
+% Functions
+%**************************************************************************
+% bhattacharyya.m - compute Bhattacharyya distance between 2 histograms
+% histnorm.m- Histogram normalized
+%**************************************************************************
+% Scripts
+%**************************************************************************
+%
+
