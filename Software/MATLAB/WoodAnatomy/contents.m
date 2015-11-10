b0662031da19ee77b10bfc8cbd7480da416f4c8e
elboyran <e.ranguelova@esciencecenter.nl>
2015-11-10 15:51:22 +0100

    added a package by B. Schauerte for computing different histogram distances from Matlab file exchange

--- Software/MATLAB/WoodAnatomy/contents.m
+++ Software/MATLAB/WoodAnatomy/contents.m
@@ -8,4 +8,7 @@
 % Testing directory- contains the scripts for testing
 %**************************************************************************
 % Tools directory- contains more generic tools
+%--------------------------------------------------------------------------
+% subdirectory histogram_distances contains the package downloaded from
+% MATLAB Central: http://www.mathworks.com/matlabcentral/fileexchange/39275-histogram-distances
 %**************************************************************************
