1e0da1662f2a0badcc5ae477fb9a780de1b5dc73
elboyran <e.ranguelova@esciencecenter.nl>
2016-02-12 15:52:13 +0100

    modified testing script  with multiple logical operators between the filtering criteria

--- Software/MATLAB/WoodAnatomy/Testing/test_bwstatsfilt.m
+++ Software/MATLAB/WoodAnatomy/Testing/test_bwstatsfilt.m
@@ -6,10 +6,8 @@ detector  = 'DMSR';
 test_case = 'Desmo';
 [image_filenames, features_filenames, regions_filenames, regions_props_filenames] = ...
        get_wood_test_filenames(test_case, detector, data_path, results_path);
-%% params   
-im_idx = 2;
-stats_types = {'Orientation', 'Eccentricity'};
-ranges = {[0 90],[0.8 1]};
+
+im_idx = 1;
 %% load
 image_filename = char(image_filenames{im_idx});
 regions_filename = char(regions_filenames{im_idx});
@@ -19,10 +17,27 @@ load(regions_props_filename);
 clear histograms regions_properties
 bw = logical(saliency_masks);
 clear saliency_masks
+
+%% params
+stats_types = {'RelativeArea'};
+range  = [0.4 1]; % the top 10% ofthe Area
+stats_values = cat(1,statistics.(char(stats_types{1})));
+max_value  = max(stats_values(:));
+lo_thr = range(1)*max_value;
+hi_thr = range(2)*max_value;
+logic_ops = {};
+ranges = {[lo_thr hi_thr]}; %{[0.01 0.015]};
+
+% stats_types = {'Orientation','Orientation','Eccentricity'};
+% logic_ops = {'Or','AND'};
+% ranges = {[-90 -60],[60 90],[0.8 1]};
+
 %% show
 imshow(bw);
+
 %% run
-[bw_filt, regions_idx, threshs] = bwstatsfilt(bw, statistics, stats_types, conn_comp, ranges);
+[bw_filt, regions_idx, threshs] = bwstatsfilt(bw, statistics, stats_types, ...
+    logic_ops, conn_comp, ranges);
 clear conn_comp
 %% show
 figure;imshow(bw_filt);
