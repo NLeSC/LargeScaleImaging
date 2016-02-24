60901f597baae8ab5c81a44293022960e3c933f1
elboyran <e.ranguelova@esciencecenter.nl>
2016-02-24 19:52:55 +0100

    how to treat NaN ellipses when visualizing

--- Software/MATLAB/AfineRegions/Detectors/SMSSR/display_smart_regions.m
+++ Software/MATLAB/AfineRegions/Detectors/SMSSR/display_smart_regions.m
@@ -19,9 +19,9 @@
 % modification details:  added figure and subplot handles as parameters
 %**************************************************************************
 % INPUTS:
-% image_fname- the original image filename 
+% image_fname- the original image filename
 % detector - string indicating the salient regions detector (S/D/MSSR)
-% feature_fname - the file with features 
+% feature_fname - the file with features
 % [ROI_mask_fname]- the ROI binary mask (*.mat) [optional]
 % [regions_fname]- the original saliency binary masks (*.mat) [optional]
 % [type] - flag- to read the features including the saliency type or not
@@ -31,7 +31,7 @@
 % [step_list_regions] - to show only fraction of the regions: step in the
 %              list, 1:step_num_regions, default is 1
 % [scaling]- ellipse scaling [optional] if left out default is 1
-% [labels]- flag - to display the region number next to it or no 
+% [labels]- flag - to display the region number next to it or no
 %               if left out default is 0 (no labels) [optional]
 % [col_ellipse]- the colour(s) of the ellipses [optional] if left out or
 %                empty default is magenta
@@ -55,15 +55,15 @@
 % REFERENCES:
 %**************************************************************************
 function display_smart_regions(image_fname, detector, features_fname, ROI_mask_fname, ...
-                  regions_fname, type, ...
-                  list_regions, step_list_regions, ...
-                  scaling, labels, col_ellipse, line_width, col_label,...
-                  original, fig, sbplt)
+    regions_fname, type, ...
+    list_regions, step_list_regions, ...
+    scaling, labels, col_ellipse, line_width, col_label,...
+    original, fig, sbplt)
 
 %**************************************************************************
-% input control    
+% input control
 %--------------------------------------------------------------------------
-if (nargin<16) 
+if (nargin<16)
     sbplt = subplot(1,1,1);
 end
 if (nargin<15) || isempty(fig)
@@ -87,7 +87,7 @@ end
 if (nargin < 9) || isempty(scaling)
     scaling = 1;
 end
-if (nargin < 8) 
+if (nargin < 8)
     list_regions = [];
 end
 if (nargin < 7) || isempty(step_list_regions)
@@ -97,10 +97,10 @@ if (nargin < 6) || isempty(type)
     type = 0;
 end
 if nargin < 5
-	regions_fname = [];  
+    regions_fname = [];
 end
 if nargin < 4
-    ROI_mask_fname = [];  
+    ROI_mask_fname = [];
 end
 if nargin < 3
     error('display_smart_regions.m requires at least 2 input arguments!');
@@ -117,9 +117,9 @@ end
 %**************************************************************************
 % input parameters -> variables
 %--------------------------------------------------------------------------
-% features 
+% features
 [num_regions, features, saliency_masks] =...
-    open_regions(detector, features_fname, regions_fname, type); 
+    open_regions(detector, features_fname, regions_fname, type);
 sprintf('There are %d number of regions detected.', num_regions)
 
 % image
@@ -138,10 +138,10 @@ if ~isempty(ROI_mask_fname)
             ROI = field;
         end
     end
-       if isempty(ROI)
-            error('ROI_mask_fname does not contain binary mask!');
-            return %#ok<UNRCH>
-       end      
+    if isempty(ROI)
+        error('ROI_mask_fname does not contain binary mask!');
+        return %#ok<UNRCH>
+    end
 end
 
 % regions
@@ -171,10 +171,10 @@ end
 if ~(isempty(ROI))
     if (ndims(I_or)==3)
         I_r=I_or(:,:,1); I_g=I_or(:,:,2); I_b=I_or(:,:,3);
-         I_r(~ROI)=1; I_g(~ROI)=1; I_b(~ROI)=1;
-         I(:,:,1)=I_r; I(:,:,2)=I_g; I(:,:,3)=I_b;
+        I_r(~ROI)=1; I_g(~ROI)=1; I_b(~ROI)=1;
+        I(:,:,1)=I_r; I(:,:,2)=I_g; I(:,:,3)=I_b;
     else
-         I = I_or; I(~ROI)=1; 
+        I = I_or; I(~ROI)=1;
     end
 else
     I = I_or;
@@ -183,30 +183,30 @@ end
 % display the original regions overlaid on the image
 if original
     Per = zeros(nrows,ncols);
-            for i=1:size(saliency_masks,3)
-                   Per = Per|bwperim(saliency_masks(:,:,i));
-            end
-            Per_vis = imdilate(Per,strel('square',line_width));
-            if (ndims(I_or)==3)
-                 I_r=I_or(:,:,1); I_g=I_or(:,:,2); I_b=I_or(:,:,3);
-                 I_r(Per_vis)=255; I_g(Per_vis)=0; I_b(Per_vis)=255; 
-                 I_reg(:,:,1)=I_r; I_reg(:,:,2)=I_g; I_reg(:,:,3)=I_b;
-                 I_reg = uint8(I_reg);
-            else
-                 I_r = I_or; I_g = I_or; I_b = I_or;
-                 I_r(Per_vis)=255; I_g(Per_vis)=0; I_b(Per_vis)=255;
-                 I_reg(:,:,1)=I_r; I_reg(:,:,2)=I_g; I_reg(:,:,3)=I_b;
-                 I_reg = uint8(I_reg);
-            end
+    for i=1:size(saliency_masks,3)
+        Per = Per|bwperim(saliency_masks(:,:,i));
+    end
+    Per_vis = imdilate(Per,strel('square',line_width));
+    if (ndims(I_or)==3)
+        I_r=I_or(:,:,1); I_g=I_or(:,:,2); I_b=I_or(:,:,3);
+        I_r(Per_vis)=255; I_g(Per_vis)=0; I_b(Per_vis)=255;
+        I_reg(:,:,1)=I_r; I_reg(:,:,2)=I_g; I_reg(:,:,3)=I_b;
+        I_reg = uint8(I_reg);
+    else
+        I_r = I_or; I_g = I_or; I_b = I_or;
+        I_r(Per_vis)=255; I_g(Per_vis)=0; I_b(Per_vis)=255;
+        I_reg(:,:,1)=I_r; I_reg(:,:,2)=I_g; I_reg(:,:,3)=I_b;
+        I_reg = uint8(I_reg);
+    end
     
     f0=figure; imshow(I_reg); axis on; title(['SMSSR for %s' image_fname]);
 end
 
 % display the (relevant part of the) image
 I = uint8(I);
-figure(fig); 
+figure(fig);
 if ~isempty(sbplt)
-        subplot(sbplt);
+    subplot(sbplt);
 end
 imshow(I);axis on; hold on;
 %--------------------------------------------------------------------------
@@ -220,57 +220,59 @@ for n=list_regions
     i = i+1;
     x = features(n,1); y = features(n,2);
     a = features(n,3); b = features(n,4); c = features(n,5);
-    if type
-        % display the 4 saliency types in different colour-coding
-        sal_type = features(n,6);
-        if sal_type == 1
-            % holes
-            col_ellipse = [0 0 1]; % blue
-        elseif sal_type == 2
-            % islands
-            col_ellipse = [1 1 0]; % yellow;
-        elseif sal_type == 3
-            %indentations            
-            col_ellipse = [0 1 0]; % green;
-        else
-            %protrusions
-            col_ellipse = [1 0 0]; % red;
-        end
-         % draw the ellipse if within the ROI
-         if ~isempty(ROI) 
-             if ROI(fix(y),fix(x))
-                 drawellipse(x,y,a,b,c, scaling, col_ellipse,line_width);
-             end
-         else
-            drawellipse(x,y,a,b,c, scaling, col_ellipse,line_width);
-         end
-    else % no type is given
-        % draw the ellipse
-        if size(col_ellipse,1) > 1
-            if ~isempty(ROI) 
-             if ROI(fix(y),fix(y))
-              drawellipse(x,y,a,b,c, scaling, col_ellipse(i,:),line_width);
-             end
+    if not(isnan(a) && isnan(b))
+        if type
+            % display the 4 saliency types in different colour-coding
+            sal_type = features(n,6);
+            if sal_type == 1
+                % holes
+                col_ellipse = [0 0 1]; % blue
+            elseif sal_type == 2
+                % islands
+                col_ellipse = [1 1 0]; % yellow;
+            elseif sal_type == 3
+                %indentations
+                col_ellipse = [0 1 0]; % green;
             else
-              drawellipse(x,y,a,b,c, scaling, col_ellipse(i,:),line_width);
-            end             
-        else
-           if ~isempty(ROI) 
-             if ROI(fix(y),fix(y)) 
-                  drawellipse(x,y,a,b,c, scaling, col_ellipse,line_width);
-             end
-           else
-               drawellipse(x,y,a,b,c, scaling, col_ellipse,line_width);
-           end                  
+                %protrusions
+                col_ellipse = [1 0 0]; % red;
+            end
+            % draw the ellipse if within the ROI
+            if ~isempty(ROI)
+                if ROI(fix(y),fix(x))
+                    drawellipse(x,y,a,b,c, scaling, col_ellipse,line_width);
+                end
+            else
+                drawellipse(x,y,a,b,c, scaling, col_ellipse,line_width);
+            end
+        else % no type is given
+            % draw the ellipse
+            if size(col_ellipse,1) > 1
+                if ~isempty(ROI)
+                    if ROI(fix(y),fix(y))
+                        drawellipse(x,y,a,b,c, scaling, col_ellipse(i,:),line_width);
+                    end
+                else
+                    drawellipse(x,y,a,b,c, scaling, col_ellipse(i,:),line_width);
+                end
+            else
+                if ~isempty(ROI)
+                    if ROI(fix(y),fix(y))
+                        drawellipse(x,y,a,b,c, scaling, col_ellipse,line_width);
+                    end
+                else
+                    drawellipse(x,y,a,b,c, scaling, col_ellipse,line_width);
+                end
+            end
         end
-    end
-    hold on;
-     % the labels
-    if labels
-        plot(x,y,[col_label, '+']);
-        text(x + offset, y + offset, num2str(n+2),...
-            'Color',col_label,'FontSize',8,'FontWeight','bold');
-        hold off;
+        hold on;
+        % the labels
+        if labels
+            plot(x,y,[col_label, '+']);
+            text(x + offset, y + offset, num2str(n+2),...
+                'Color',col_label,'FontSize',8,'FontWeight','bold');
+            hold off;
+        end
     end
 end
 
@@ -281,32 +283,32 @@ end
 %**************************************************************************
 % subfunstions
 %--------------------------------------------------------------------------
-function drawellipse(x,y,a,b,c,scaling,col,lw)
-    % draws the ellipse corresponding the each feature 
-    figure(fig); 
-    if ~isempty(sbplt)
-        subplot(sbplt);
+    function drawellipse(x,y,a,b,c,scaling,col,lw)
+        % draws the ellipse corresponding the each feature
+        figure(fig);
+        if ~isempty(sbplt)
+            subplot(sbplt);
+        end
+        hold on;
+        [v e]=eig([a b;b c]);
+        
+        l1=1/sqrt(e(1));
+        l2=1/sqrt(e(4));
+        
+        alpha=atan2(v(4),v(3));
+        t = 0:pi/50:2*pi;
+        yt=scaling*(l2*sin(t));
+        xt=scaling*(l1*cos(t));
+        
+        p=[xt;yt];
+        R=[cos(alpha) sin(alpha);-sin(alpha) cos(alpha)];
+        pt=R*p;
+        % first plot black ellipse and then narrower on top with the desired colour
+        plot(pt(2,:)+x,pt(1,:)+y,'k-','LineWidth',1.5*lw);
+        plot(pt(2,:)+x,pt(1,:)+y,'w-','LineWidth',lw);
+        set(findobj(gca,'Type','line','Color',[1 1 1]),...
+            'Color',col);
+        hold off;
     end
-    hold on;
-    [v e]=eig([a b;b c]);
-
-    l1=1/sqrt(e(1));
-    l2=1/sqrt(e(4));
-
-    alpha=atan2(v(4),v(3));
-    t = 0:pi/50:2*pi;
-    yt=scaling*(l2*sin(t));
-    xt=scaling*(l1*cos(t));
-
-    p=[xt;yt];
-    R=[cos(alpha) sin(alpha);-sin(alpha) cos(alpha)];
-    pt=R*p;
-    % first plot black ellipse and then narrower on top with the desired colour
-    plot(pt(2,:)+x,pt(1,:)+y,'k-','LineWidth',1.5*lw);
-    plot(pt(2,:)+x,pt(1,:)+y,'w-','LineWidth',lw);
-    set(findobj(gca,'Type','line','Color',[1 1 1]),...
-        'Color',col);
-    hold off;
-end
 
 end
