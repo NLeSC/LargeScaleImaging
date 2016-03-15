6053d0b4a2b7e0caa5eb40e96eaf38489e3aad85
elboyran <e.ranguelova@esciencecenter.nl>
2016-03-15 16:31:15 +0100

    function to get the filtered regions centroids out of the saved MAT structures

--- Software/MATLAB/WoodAnatomy/Testing/get_filtered_regions_centroids.m
+++ Software/MATLAB/WoodAnatomy/Testing/get_filtered_regions_centroids.m
@@ -23,3 +23,24 @@
 %**************************************************************************
 % REFERENCES:
 %**************************************************************************
+function [list_centroids] = get_filtered_regions_centroids(regions_props_filename, ...
+                                              filtered_regions_filename)
+%% argument check                                          
+if nargin < 2
+    error('get_filtered_regions_centroids requires 2 input arguments!');
+end                                       
+
+
+%% get the centroids form the data in the input files
+
+% get the indicies of the filtered regions
+load(filtered_regions_filename,'filtered_regions_idx'); 
+
+% get the regions properties of all regions
+load(regions_props_filename, 'regions_properties');
+
+% get the centroids of all regions
+all_centroids = cat(1,regions_properties.Centroid);
+
+% get only the filtered centroids
+list_centroids = all_centroids(filtered_regions_idx,:);
