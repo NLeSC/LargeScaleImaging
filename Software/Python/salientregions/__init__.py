c5c8fe98a582b7b4ff7ae5145019f596221a0e1c
dafnevk <d.vankuppevelt@esciencecenter.nl>
2016-03-16 18:07:42 +0100

    added a function for vizualizing regions

--- Software/Python/salientregions/__init__.py
+++ Software/Python/salientregions/__init__.py
@@ -1,2 +1,2 @@
-from helpers import show_image, binarize, read_matfile, image_diff, get_SE
+from helpers import show_image, binarize, read_matfile, image_diff, get_SE, vizualize_elements
 from binarydetector import get_holes, get_islands, fill_image, remove_small_elements, get_protrusions, get_indentations
