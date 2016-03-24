88e9be528c047ac78bb289eb77b70ba6aaf3af4d
dafnevk <d.vankuppevelt@esciencecenter.nl>
2016-03-24 15:47:55 +0100

    Moved Data driven binary detector to helpers, that seems more logical.

--- Software/Python/salientregions/__init__.py
+++ Software/Python/salientregions/__init__.py
@@ -1,3 +1,3 @@
-from helpers import show_image, binarize, read_matfile, image_diff, get_SE, get_SEhi, visualize_elements
+from helpers import show_image, binarize, read_matfile, image_diff, get_SE, get_SEhi, visualize_elements, data_driven_binarization
 from binarydetector import get_holes, get_islands, fill_image, remove_small_elements, get_protrusions, get_indentations, get_salient_regions
-from salientregiondetector import data_driven_binarization
+from salientregiondetector import get_salient_regions_gray, get_salient_regions_color
--- Software/Python/salientregions/helpers.py
+++ Software/Python/salientregions/helpers.py
@@ -218,3 +218,65 @@ def get_SEhi(SE, lam, scaleSE=2, scalelam=10):
     SEhi = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (int(SE.shape[0]/scaleSE), int(SE.shape[1]/scaleSE)))
     lamhi = lam/scalelam
     return SEhi, lamhi
+    
+def data_driven_binarization(img, area_factor_large=0.001, area_factor_verylarge=0.1, lam=-1, weights=(0.33,0.33,0.33), connectivity=4, visualize=True):
+    '''
+    Binarize the image such that the desired number of (large) connected 
+    components is maximized.
+    
+    Parameters:
+    ------
+    img: 2-dimensional numpy array with values between 0 and 255
+        grayscale image to binarize
+    area_factor_large: float, optional
+        factor that describes the minimum area of a large CC
+    area_factor_verylarge: float, optional
+        factor that describes the minimum area of a very large CC
+    lam: float, optional
+        lambda, minimumm area of a connected component
+    weights: (float, float, float)
+        weights for number of CC, number of large CC and number of very large CC
+        respectively.
+    connectivity: int
+        What connectivity to use to define CCs
+    visualize: bool, optional
+        option for vizualizing the process
+    
+    Returns:
+    ------ 
+    t_opt: int
+        optimal threshold
+    binarized: 2-dimensional numpy array with values 0/255
+        The binarized image
+    '''
+    area = img.size
+    if lam == -1:
+        _, lam = get_SE(img) 
+    area_large = area_factor_large*area
+    area_verylarge = area_factor_verylarge*area
+    
+    if visualize:
+        print 'lambda is: %i' % lam
+        print 'Area large is: %i' % area_large
+        print 'Area very large is: %i' % area_verylarge
+    
+    #Initialize the
+    a_nccs = np.zeros(256)
+    a_nccs_large = np.zeros(256)
+    a_nccs_verylarge = np.zeros(256)
+    for t in xrange(256) :
+        bint = binarize(img, threshold=t, visualize=False)
+        nccs, labels, stats, centroids = cv2.connectedComponentsWithStats(bint, connectivity=connectivity)
+        areas = stats[:, cv2.CC_STAT_AREA]
+        a_nccs[t] = sum(areas > lam)
+        a_nccs_large[t] = sum(areas > area_large)
+        a_nccs_verylarge[t] = sum(areas > area_verylarge)
+
+    #Normalize
+    a_nccs = a_nccs/float(a_nccs.max())
+    a_nccs_large = a_nccs_large/float(a_nccs_large.max())
+    a_nccs_verylarge = a_nccs_verylarge/float(a_nccs_verylarge.max())
+    scores = weights[0]*a_nccs + weights[1]*a_nccs_large + weights[2]*a_nccs_verylarge
+    t_opt = scores.argmax()
+    binarized = binarize(img, threshold=t_opt, visualize=visualize)
+    return t_opt, binarized
