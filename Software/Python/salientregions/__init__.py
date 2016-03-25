076f431cd50ce3f41e38bbd25e85459aa8ac7ab7
dafnevk <d.vankuppevelt@esciencecenter.nl>
2016-03-25 17:17:29 +0100

    Added setup.py file and adjusted makefile so that it can also install the salientregion package

--- Software/Python/salientregions/__init__.py
+++ Software/Python/salientregions/__init__.py
@@ -1,3 +1,5 @@
 from helpers import show_image, binarize, read_matfile, image_diff, get_SE, get_SEhi, visualize_elements, data_driven_binarization
 from binarydetector import get_holes, get_islands, fill_image, remove_small_elements, get_protrusions, get_indentations, get_salient_regions
 from salientregiondetector import get_salient_regions_gray, get_salient_regions_color
+
+__all__ = ['helpers', 'binarydetector', 'salientregiondetector']
