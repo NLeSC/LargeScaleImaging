6a07778237ab03b9b66bc36a85edc4937d87c3c5
dafnevk <d.vankuppevelt@esciencecenter.nl>
2016-04-01 09:52:39 +0200

    Added MSSR detector

--- Software/Python/salientregions/__init__.py
+++ Software/Python/salientregions/__init__.py
@@ -4,7 +4,7 @@ from helpers import show_image, binarize, read_matfile, \
 from binarydetector import get_holes, get_islands, fill_image, \
                            remove_small_elements, get_protrusions, \
                            get_indentations, get_salient_regions_binary
-from salientregiondetector import get_salient_regions
+from salientregiondetector import get_salient_regions, get_salient_regions_MSSR
 from binarization import Binarizer, ThresholdBinarizer, \
                         OtsuBinarizer, DatadrivenBinarizer
 
--- Software/Python/salientregions/salientregiondetector.py
+++ Software/Python/salientregions/salientregiondetector.py
@@ -55,7 +55,7 @@ def get_salient_regions(img,
     # Convert to grayscale if the image has 3 channels
     if len(img.shape) == 3:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    
+
     SE, lam = helpers.get_SE(img, SE_size_factor=SE_size_factor)
     # The default binarizer is the Data Driven binarizer
     if binarizer is None:
@@ -63,7 +63,7 @@ def get_salient_regions(img,
                                                      connectivity=connectivity)
     # Binarize the image
     binarized = binarizer.binarize(img, visualize)
-    
+
     # Find regions in the binary image
     result = binarydetector.get_salient_regions_binary(binarized,
                                                        find_holes,
@@ -75,3 +75,125 @@ def get_salient_regions(img,
                                                        connectivity,
                                                        visualize)
     return result
+
+
+def get_salient_regions_MSSR(img,
+                             find_holes=True, find_islands=True,
+                             find_indentations=True, find_protrusions=True,
+                             SE_size_factor=0.15, area_factor=0.05,
+                             connectivity=4,
+                             binarizer=None,
+                             perc=0.7,
+                             min_thres=0,
+                             max_thres=255,
+                             step=1,
+                             visualize=True):
+    '''
+    Find salient regions of all four types using MSSR
+
+    Parameters:
+    ------
+    img: 2-dimensional numpy array with values between 0 and 255
+        grayscale image to detect regions
+    find_holes: bool, optional
+        Whether to detect regions of type hole
+    find_islands: bool, optional
+        Whether to detect regions of type island
+    find_indentations: bool, optional
+        Whether to detect regions of type indentation
+    find_protrusions: bool, optional
+        Whether to detect regions of type protrusion
+    SE_size_factor: float, optional
+        The fraction of the image size that the structuring element should be
+    area_factor: float, optional
+        factor that describes the minimum area of a significent CC
+    connectivity: int
+        What connectivity to use to define CCs
+    perc: float, optional
+        The percentile at which the threshold for the final regions is taken
+    min_thres: int, optional
+        Minimum threshold level
+    max_thres: int, optional
+        Maximum threshold level
+    step: int, optional
+        stepsize for looping through threshold levels
+    visualize: bool, optional
+        option for vizualizing the process
+
+    Returns:
+    ------
+    dictiornary with the following possible items:
+    holes: 2-dimensional numpy array with values 0/255
+        Image with all holes as foreground.
+    islands: 2-dimensional numpy array with values 0/255
+        Image with all islands as foreground.
+    indentations: 2-dimensional numpy array with values 0/255
+        Image with all indentations as foreground.
+    protrusions: 2-dimensional numpy array with values 0/255
+        Image with all protrusions as foreground.
+    '''
+    # Convert to grayscale if the image has 3 channels
+    if len(img.shape) == 3:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    result = {}
+    if find_holes:
+        result['holes'] = np.zeros(img.shape, dtype='uint8')
+    if find_islands:
+        result['islands'] = np.zeros(img.shape, dtype='uint8')
+    if find_indentations:
+        result['indentations'] = np.zeros(img.shape, dtype='uint8')
+    if find_protrusions:
+        result['protrusions'] = np.zeros(img.shape, dtype='uint8')
+
+    for t in xrange(min_thres, max_thres, step):
+        _, bint = cv2.threshold(img, t, 255, cv2.THRESH_BINARY)
+        regions = binarydetector.get_salient_regions_binary(bint,
+                                                            find_holes,
+                                                            find_islands,
+                                                            find_indentations,
+                                                            find_protrusions,
+                                                            SE_size_factor,
+                                                            area_factor,
+                                                            connectivity,
+                                                            visualize=False)
+        for regtype in result.keys():
+            result[regtype] += np.array(1 *
+                                        (regions[regtype] > 0), dtype='uint8')
+
+    for regtype in result.keys():
+        if visualize:
+            helpers.show_image(
+                result[regtype],
+                regtype + " before thresholding")
+        result[regtype] = threshold_cumsum(result[regtype], perc=perc)
+        if visualize:
+            helpers.show_image(
+                result[regtype],
+                regtype + " after thresholding")
+
+    return result
+
+
+def threshold_cumsum(data, perc=0.7, maxvalue=255):
+    '''
+    Thresholds an image based on a percentile of the non-zero pixel values.
+
+    Parameters:
+    ------
+    data: 2-dimensional numpy array
+        the image to threshold
+    perc: float, optional
+        The percentile at which the threshold is taken
+    maxvalue: int, optional
+        The value that the thresholded pixels are set to
+
+    Returns:
+    ------
+    binarized: 2-dimensional numpy array
+        Thresholded image
+    '''
+    data_values = data[data > 0]
+    thres = np.percentile(data_values, int(perc * 100))
+    _, binarized = cv2.threshold(data, thres, maxvalue, cv2.THRESH_BINARY)
+    return binarized
