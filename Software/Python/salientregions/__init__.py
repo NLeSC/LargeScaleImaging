bc9120c03986acf6267169c4f9a72aa982aeb592
dafnevk <d.vankuppevelt@esciencecenter.nl>
2016-04-07 17:25:37 +0200

    Added detector to init

--- Software/Python/salientregions/__init__.py
+++ Software/Python/salientregions/__init__.py
@@ -2,7 +2,7 @@ from helpers import show_image, binarize, read_matfile, \
     image_diff,  visualize_elements, \
     data_driven_binarization
 from binarydetector import BinaryDetector
-from detectors import SalientDetector
+from detectors import SalientDetector, SalientDetector
 from binarization import Binarizer, ThresholdBinarizer, \
     OtsuBinarizer, DatadrivenBinarizer
 
