1cfc907c4fa7f6186780a4018fe0799747d5072b
elboyran <e.ranguelova@esciencecenter.nl>
2016-07-29 18:42:40 +0200

    Descriptor directory and contents template

---
+++ Software/MATLAB/Descriptors/contents.m
@@ -0,0 +1,11 @@
+% contents.m- contents of directory ...\Descriptors
+%
+% topic: Salient regions description
+% author: Elena Ranguelova, NLeSc
+% date: July 2016
+%
+%**************************************************************************
+% Moments- contains the code for the 2D Binary complex moment invariants
+% 
+% Testing directory- contains the scripts for testing
+%**************************************************************************
