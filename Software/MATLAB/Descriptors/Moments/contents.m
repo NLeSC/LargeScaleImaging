97d8fb1322796cd54513d34a424d7576c1fe04ca
elboyran <e.ranguelova@esciencecenter.nl>
2016-08-02 17:20:19 +0200

    updating descriptos contents

--- Software/MATLAB/Descriptors/Moments/contents.m
+++ Software/MATLAB/Descriptors/Moments/contents.m
@@ -15,5 +15,6 @@
 % secondary
 %--------------------------------------------------------------------------
 % flusser_moment.m- computing complex central moment proposed by Flusser
-% scale_moment_invariants
+% scale_moment_invariant.m- computing a scale moment invariant proposed by Flusser
+% scale_moment_invariants - 
 % rotation_moment_invariants
