8d20822ab2cc7623d225068b5a1ca949f3193351
elboyran <e.ranguelova@esciencecenter.nl>
2016-08-03 18:34:25 +0200

    testing script

--- Software/MATLAB/Testing/test_scale_moment_invariant.m
+++ Software/MATLAB/Testing/test_scale_moment_invariant.m
@@ -1,17 +1,17 @@
 % test_scale_moment_invariant.m- script for testing scale moment invariant computation
 %**************************************************************************
 % author: Elena Ranguelova, NLeSc
-% date created: 2-0-2016
+% date created: 2-08-2016
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% last modification date:
-% modification details:
+% last modification date: 3-08-2016
+% modification details: tetsing scale invariants on a scaled image
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % NOTE:
 %**************************************************************************
 
 %% define some parameters
 % execution parameters
-verbose = 1;
+verbose = 0;
 vis = 1;
 
 % CCes and properties
@@ -25,21 +25,27 @@ v = 1;
 
 %% load a simple binary test image
 if verbose
-    disp('Loading and binarising a test image...');
+    disp('Loading a binary test image and a transformed image...');
 end
 
-I = imread('circlesBrightDark.png');
+bw = imread('Binary_islands.png');
+bw = logical(bw);
+bwtr = imread('BinaryIslands_oxfrei_scale3trans.png');
+bwtr = logical(bwtr);
 
-if vis
-    f = figure; subplot(221); imshow(I); title('Test image'); axis on, grid on;
-end
+
+
+% if vis
+%     f = figure; subplot(221); imshow(I); title('Test image'); axis on, grid on;
+%     subplot(222); imshow(Itr); title('Test image: scaled'); axis on, grid on;
+% end
 
 %% binarise the image
-bw = (I > 200) | (I < 100);
 
 % visualise
 if vis
-    figure(f); subplot(222);imshow(bw); title('Binarised'); axis on, grid on;
+    f = figure; subplot(221);imshow(bw); title('Binary'); axis on, grid on;
+    subplot(222);imshow(bwtr); title('Binary (transf.)'); axis on, grid on;
 end
 
 
@@ -48,6 +54,7 @@ if verbose
     disp('Obtain the connected components...');
 end
 [regions_properties, conn_comp] = compute_region_props(bw, conn, list_properties);
+[regions_properties_tr, conn_comp_tr] = compute_region_props(bwtr, conn, list_properties);
 
 % visualise
 if vis
@@ -55,32 +62,60 @@ if vis
     labeled = labelmatrix(cc);
     RGB_label = label2rgb(labeled);
     figure(f); subplot(223);imshow(RGB_label); title('Connected Components'); axis on, grid on;
+    cc_tr = bwconncomp(bwtr);
+    labeled = labelmatrix(cc_tr);
+    RGB_label = label2rgb(labeled);
+    subplot(224);imshow(RGB_label); title('Connected Components - transformed'); axis on, grid on;
 end
 
 %% compute scale moments invariants of all CCs
 
 num_regions = length(cat(1,regions_properties.Area));
-moment_invariant = 0;
+moment_invariant = zeros(1, num_regions);
 
-%for region_idx = 1: num_regions
-region_idx = input('Enter a region index: ');
-
-if verbose
-    disp('Processing region #: '); disp(region_idx);
+for region_idx = 1: num_regions
+    %region_idx = input('Enter a region index: ');
+    
+    if verbose
+        disp('Processing region #: '); disp(region_idx);
+    end
+    pixel_list = regions_properties(region_idx).PixelList;
+    centroid = regions_properties(region_idx).Centroid;
+    area = regions_properties(region_idx).Area;
+    
+    if verbose
+        disp('Compute the scale moment...');
+    end
+    
+    [moment_invariant(region_idx)] = scale_moment_invariant(u, v, pixel_list, ...
+        centroid, area);
+    if verbose
+        disp('Scale moment invariant: '); disp(moment_invariant);
+    end
 end
-pixel_list = regions_properties(region_idx).PixelList;
-centroid = regions_properties(region_idx).Centroid;
-area = regions_properties(region_idx).Area;
 
-if verbose
-    disp('Compute the scale moment...');
+num_regions_tr = length(cat(1,regions_properties_tr.Area));
+moment_invariant_tr = zeros(1, num_regions_tr);
+
+for region_idx_tr = 1: num_regions_tr
+    %region_idx_tr = input('Enter a region index for the transformed image: ');
+    
+    if verbose
+        disp('Processing region #: '); disp(region_idx_tr);
+    end
+    pixel_list = regions_properties_tr(region_idx_tr).PixelList;
+    centroid = regions_properties_tr(region_idx_tr).Centroid;
+    area = regions_properties_tr(region_idx_tr).Area;
+    
+    if verbose
+        disp('Compute the scale moment...');
+    end
+    
+    [moment_invariant_tr(region_idx_tr)] = scale_moment_invariant(u, v, pixel_list, ...
+        centroid, area);
+    if verbose
+        disp('Scale moment invariant (transf.): '); disp(moment_invariant_tr);
+    end
+    
 end
-
-[moment_invariant] = scale_moment_invariant(u, v, pixel_list, ...
-    centroid, area);
-if verbose
-    disp('Scale moment invariant: '); disp(moment_invariant);
-end
-
-%end
 
