334d8ec0ca88e4c853724e6dd76b456ca07d2546
elboyran <e.ranguelova@esciencecenter.nl>
2016-08-08 15:09:52 +0200

    the RTS testing script can work nowalso on multiple regions wihtin 1 image

--- Software/MATLAB/Testing/test_rotmi.m
+++ Software/MATLAB/Testing/test_rotmi.m
@@ -2,10 +2,10 @@
 %                                          (rotaiton translation and scaling)
 %**************************************************************************
 % author: Elena Ranguelova, NLeSc
-% date created: 5-08-2016
+% date created: 05-08-2016
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% last modification date:
-% modification details:
+% last modification date: 08-08-2016
+% modification details: the script can test single or multiple regions
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % NOTE:
 %**************************************************************************
@@ -18,7 +18,8 @@
 % execution parameters
 verbose = 1;
 vis = 1;
-
+% the test image contains multiple binary shapes?
+multiple = input('Test image with muliple regions? [1/0]: ');
 % moments order
 order = input('Enter the order of the moments: ');
 
@@ -27,69 +28,89 @@ if verbose
     disp('Loading a binary test image and transforming it...');
 end
 
-%bw = imread('Binary_islands.png');
-bw = rgb2gray(imread('blob.png'));
+if multiple
+    bw = imread('Binary_islands.png');
+else
+    bw = rgb2gray(imread('blob.png'));
+end
 bw = logical(bw);
 % define translation matrix
-dx = 25; dy = 35;
+dx = 15; dy = 35;
 Ht = [1 0 dx; 0 1 dy; 0 0 1];
 % define scaling matrix
 sx = 2; sy = 2;
 Hs = [sx 0 0; 0 sy 0; 0 0 1];
-% define sheer
-ssx = 0.5; ssy = 0.2;
-Hsh = [ssx ssy 0; 0 ssy 0; 0 0 1];
+% % define sheer
+% ssx = 0.5; ssy = 0.2;
+% Hsh = [ssx ssy 0; 0 ssy 0; 0 0 1];
 % define rotation matrix
 theta = deg2rad(30);
 cos_theta = cos(theta); sin_theta = sin(theta);
 Hr = [cos_theta -sin_theta 0; sin_theta cos_theta 0; 0 0 1];
-% define affine matrix
-Ha = Hr*Hsh*Hs*Ht;
+% % define affine matrix
+% Ha = Hr*Hsh*Hs*Ht;
 
 %  obtain transformed images
-bw_t = logical(applyAffineTransform(bw, Ht', 0));
+bw_t = logical(applyAffineTransform(bw, Ht', 1));
 bw_s = logical(applyAffineTransform(bw, Hs', 0));
-bw_sh = logical(applyAffineTransform(bw, Hsh', 0));
+%bw_sh = logical(applyAffineTransform(bw, Hsh', 0));
 bw_r = logical(applyAffineTransform(bw, Hr', 0));
-bw_a = logical(applyAffineTransform(bw, Ha', 0));
+%bw_a = logical(applyAffineTransform(bw, Ha', 0));
 
 % visualise
 if vis
-    f = figure; subplot(321);imshow(bw); title('Binary'); axis on, grid on;
-    %subplot(323);imshow(bw_t); title('Binary (translation)'); axis on, grid on;
-    subplot(323);imshow(bw_s); title('Binary (scaling)'); axis on, grid on;
-    subplot(324);imshow(bw_r); title('Binary (rotation)'); axis on, grid on;
-    subplot(325);imshow(bw_sh); title('Binary (sheer)'); axis on, grid on;
-    subplot(326);imshow(bw_a); title('Binary (affine)'); axis on, grid on;
+    f = figure; subplot(221);imshow(bw); title('Binary'); axis on, grid on;
+    subplot(222);imshow(bw_t); title('Binary (translation)'); axis on, grid on;
+    subplot(223);imshow(bw_s); title('Binary (scaling)'); axis on, grid on;
+    subplot(224);imshow(bw_r); title('Binary (rotation)'); axis on, grid on;
+    %   subplot(325);imshow(bw_sh); title('Binary (sheer)'); axis on, grid on;
+    %   subplot(326);imshow(bw_a); title('Binary (affine)'); axis on, grid on;
 end
 
+%% obtain connected components
+if verbose
+    disp('Obtain the connected components...');
+end
+cc = bwconncomp(bw);
+cc_t = bwconncomp(bw_t);
+cc_s = bwconncomp(bw_s);
+cc_r = bwconncomp(bw_r);
+%cc_sh = bwconncomp(bw_sh);
+%cc_a = bwconncomp(bw_a);
 
-% %% obtain connected components
-% if verbose
-%     disp('Obtain the connected components...');
-% end
-% [regions_properties, conn_comp] = compute_region_props(bw, conn, list_properties);
-% [regions_properties_tr, conn_comp_tr] = compute_region_props(bwtr, conn, list_properties);
-%
-% % visualise
-% if vis
-%     cc = bwconncomp(bw);
-%     labeled = labelmatrix(cc);
-%     RGB_label = label2rgb(labeled);
-%     figure(f); subplot(223);imshow(RGB_label); title('Connected Components'); axis on, grid on;
-%     cc_tr = bwconncomp(bwtr);
-%     labeled = labelmatrix(cc_tr);
-%     RGB_label = label2rgb(labeled);
-%     subplot(224);imshow(RGB_label); title('Connected Components - rotated'); axis on, grid on;
-% end
+% visualise
+if vis
+    labeled = labelmatrix(cc);
+    f1 = figure; subplot(221);imshow(label2rgb(labeled)); title('Connected Components'); axis on, grid on;
+    
+    labeled = labelmatrix(cc_t);
+    subplot(222);imshow(label2rgb(labeled)); title('Connected Components (translation)'); axis on, grid on;
+    
+    labeled = labelmatrix(cc_s);
+    subplot(223);imshow(label2rgb(labeled)); title('Connected Components (scaling)'); axis on, grid on;
+    
+    labeled = labelmatrix(cc_r);
+    subplot(224);imshow(label2rgb(labeled)); title('Connected Components (rotation)'); axis on, grid on;
+    
+    %         labeled = labelmatrix(cc_sh);
+    %         subplot(325);imshow(label2rgb(labeled)); title('Connected Components (sheer)'); axis on, grid on;
+    %
+    %         labeled = labelmatrix(cc_a);
+    %         subplot(326);imshow(label2rgb(labeled)); title('Connected Components (affine)'); axis on, grid on;
+end
 
 %% compute scale moments invariants of all CCs
-
 if verbose
     disp('Processing original image ... ');
-    disp('Compute the moments...');
 end
-[moment_invariants] = rotmi(bw, order);
+
+num_regions = cc.NumObjects;
+
+for i = 1:num_regions
+    bwi = zeros(size(bw));
+    bwi(cc.PixelIdxList{i}) = 1;
+    [moment_invariants(i,:)] = rotmi(bwi, order);    %#ok<*SAGROW>
+end
 if verbose
     disp('Moment invariants: '); disp(moment_invariants);
     disp('--------------------------------------------');
@@ -97,44 +118,60 @@ end
 
 if verbose
     disp('Processing translated image ... ');
-    disp('Compute the moments...');
 end
-[moment_invariants_t] = rotmi(bw_t, order);
+%num_regions = cc_t.NumObjects;
+for i = 1:num_regions
+    bwi = zeros(size(bw_t));
+    bwi(cc_t.PixelIdxList{i}) =1;
+    [moment_invariants_t(i,:)] = rotmi(bwi, order);    %#ok<*SAGROW>
+end
 if verbose
     disp('Moment invariants: '); disp(moment_invariants_t);
     disp('--------------------------------------------');
 end
 
 if verbose
-    disp('Processing scaled image ... ');
-    disp('Compute the moments...');
-end
-[moment_invariants_s] = rotmi(bw_s, order);
-if verbose
-    disp('Moment invariants: '); disp(moment_invariants_s);
-    disp('--------------------------------------------');
-end
-
-if verbose
     disp('Processing rotated image ... ');
-    disp('Compute the moments...');
 end
-[moment_invariants_r] = rotmi(bw_r, order);
+%num_regions = cc_r.NumObjects;
+for i = 1:num_regions
+    bwi = zeros(size(bw_r));
+    bwi(cc_r.PixelIdxList{i}) =1;
+    [moment_invariants_r(i,:)] = rotmi(bwi, order);    %#ok<*SAGROW>
+end
 if verbose
     disp('Moment invariants: '); disp(moment_invariants_r);
     disp('--------------------------------------------');
 end
 
+if verbose
+    disp('Processing scaled image ... ');
+end
+%num_regions = cc_t.NumObjects;
+for i = 1:num_regions
+    bwi = zeros(size(bw_s));
+    bwi(cc_s.PixelIdxList{i}) =1;
+    [moment_invariants_s(i,:)] = rotmi(bwi, order);    %#ok<*SAGROW>
+end
+if verbose
+    disp('Moment invariants: '); disp(moment_invariants_s);
+    disp('--------------------------------------------');
+end
+
 %% visualize the moments as features
 if vis
-    num_moments = length(moment_invariants);
+    num_moments = size(moment_invariants,2);
     figure;
-    plot(1:num_moments, moment_invariants, 'k.',...
-        1:num_moments, moment_invariants_t, 'g+',...
-        1:num_moments, moment_invariants_s, 'm*',...
-        1:num_moments, moment_invariants_r, 'bs');
-   legend('original', 'translaton','scaling', 'rotation');     
-   title(['RTS Moment invariants for the 2D binary shape']);
-   axis on; grid on;
-        
+    for j = 1:num_regions
+        plot(1:num_moments, moment_invariants(j,:), 'ko-',...
+            1:num_moments, moment_invariants_t(j,:), 'g:+',...
+            1:num_moments, moment_invariants_s(j,:), 'm-.*',...
+            1:num_moments, moment_invariants_r(j,:), 'b--s');
+        hold on
+        legend({'original', 'translation','scaling', 'rotation'});
+    end
+    hold off
+    title(['RTS Moment invariants for the 2D binary shape']);
+    axis on; grid on;
+    
 end
