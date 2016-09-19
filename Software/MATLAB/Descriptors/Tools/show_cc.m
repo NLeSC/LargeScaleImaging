13fd0da9fdfdd2714325e2a14398f8425c8ec9b9
elboyran <e.ranguelova@esciencecenter.nl>
2016-09-19 18:01:50 +0200

    added new parameter, the indicies of the regions to be shown

--- Software/MATLAB/Descriptors/Tools/show_cc.m
+++ Software/MATLAB/Descriptors/Tools/show_cc.m
@@ -1,16 +1,17 @@
 % show_cc - visualize CCs in a given figure/subplot with title w or w/o label
 % **************************************************************************
-% [label_mat, centr_cc] = show_cc(cc, show_label, hfig, hsbplt, title_str)
+% [label_mat, centr_cc] = show_cc(cc, show_label, idx, hfig, hsbplt, title_str)
 %
 % author: Elena Ranguelova, NLeSc
 % date created: 15 Sep 2016
-% last modification date:
-% modification details:
+% last modification date: 19 Sep 2016
+% modification details: added a new parameter- vector of region indecies to be shown
 %**************************************************************************
 % INPUTS:
 % cc            connected components (as returned by bwconncomp(binary image)
 % [show_label]  logical flag. If true the CC index is shown at the centroid
 %               of each CC. Optional. Default is true.
+% [idxs]        vector of region indecies to be shown. Optional.
 % [hfig]        figure handle. Optional. If not given new figure is created
 % [hsbplt]      subplot. Optional. If not given (111) is assumed, i.e. no subplot.
 % [title_str]   title string. Optional. If not given no title is displayed.
@@ -27,17 +28,20 @@
 %**************************************************************************
 % REFERENCES:
 %**************************************************************************
-function [label_mat, centr_cc] = show_cc(cc, show_label, hfig, hsbplt, title_str)
+function [label_mat, centr_cc] = show_cc(cc, show_label,idxs, hfig, hsbplt, title_str)
 
 %% input parameters
-if nargin < 5
+if nargin < 6
     title_str =[]; % no title
 end
-if nargin < 4
+if nargin < 5
     hsbplt = subplot(111);
 end
-if nargin < 3
+if nargin < 4
     hfig = figure;
+end
+if nargin < 3
+    idxs =[];
 end
 if nargin < 2
     show_label = true;
@@ -45,6 +49,14 @@ end
 if nargin < 1
     error('show_cc requires min. 1 input argument!');
 end
+
+%% input parameters -> variables
+if isempty(idxs)
+    num_regions = -1;
+else
+    num_regions = length(idxs);    
+end
+
 %% initializations
 figure(hfig);
 subplot(hsbplt);
@@ -53,6 +65,10 @@ centr_cc = struct();
 %% computations
 label_mat = labelmatrix(cc);
 
+if num_regions < 0
+    num_regions = cc.NumObjects;
+end
+
 %% visualize
 imshow(label2rgb(label_mat));
 axis on; grid on;
@@ -60,14 +76,19 @@ axis on; grid on;
 if show_label
     centr_cc = regionprops(cc,'Centroid');
     hold on;
-    for k = 1:cc.NumObjects
-        if cc.NumObjects > 3 && k <= 2
+    for k = 1:num_regions
+        if num_regions > 10 && k <= 5
             col = 'm';
         else
             col = 'k';
         end
+        if isempty(idxs)
+            ind = k;
+        else
+            ind = idxs(k);
+        end
         text(centr_cc(k).Centroid(1), centr_cc(k).Centroid(2), ...
-            num2str(k), 'Color', col, 'HorizontalAlignment', 'center');
+            num2str(ind), 'Color', col, 'HorizontalAlignment', 'center');
     end
     hold off;
 end
