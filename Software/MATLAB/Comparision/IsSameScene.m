7d43c9dff49f1d10f49fa1afe86525b3d573de88
elboyran <e.ranguelova@esciencecenter.nl>
2016-10-06 18:01:20 +0200

    extended function header

--- Software/MATLAB/Comparision/IsSameScene.m
+++ Software/MATLAB/Comparision/IsSameScene.m
@@ -1,20 +1,26 @@
 % IsSameScene-  comparing if 2 images are of the same scene
 % **************************************************************************
-% [is_same] = IsSameScene(im1, im2, exec_params,oments_params, cc_params,...
-%                         match_params)
+% [is_same, matches_ratio, dist] = IsSameScene(im1, im2, ...
+%                               exec_params, moments_params, cc_params,...
+%                               match_params)
 %
 % author: Elena Ranguelova, NLeSc
-% date created: 3 October 2016
+% date created: 4 October 2016
 % last modification date: 
 % modification details: 
 %**************************************************************************
 % INPUTS:
-% im1/2         teo input gray/color or binary images to be compared
-
+% im1/2         the input gray/color or binary images to be compared
+% exec_params   the execution parameters 
+% moments_params parameters related to the moment invariants
+% cc_params     parameters related to the connected components
+% match_params   the matching parameters 
 %**************************************************************************
 % OUTPUTS:
 % is_same       binary flag, true if images are the same and false
 %               otherwise
+% matches_ratio ratio of good matches and all matches
+% dist          distance between the 2 images
 %**************************************************************************
 % NOTES: 
 %**************************************************************************
@@ -24,5 +30,5 @@
 %**************************************************************************
 % REFERENCES:
 %**************************************************************************
-function [bw_filt, regions_idx, threshs] = filter_regions(bw, region_props, ...
-                                   prop_types, logic_ops, ranges, conn_comp)
+function [is_same, matches_ratio, dist] = IsSameScene(im1, im2,...
+                        exec_params,oments_params, cc_params,match_params)
