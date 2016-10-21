22d4b2ec0b5ab19444dd7e20e2b97751f5e27031
elboyran <e.ranguelova@esciencecenter.nl>
2016-10-21 11:34:27 +0200

    added image filenames extension as optional parameter

--- Software/MATLAB/Testing/general/display_oxford_dataset_structured.m
+++ Software/MATLAB/Testing/general/display_oxford_dataset_structured.m
@@ -1,15 +1,17 @@
 % display_oxford_dataset_structured.m - function to display the affine dataset
+
 %                                       (structured images subset)
 %**************************************************************************
-% [] = display_oxford_dataset_structured(path)
+% [] = display_oxford_dataset_structured(path, ext)
 %
 % author: Elena Ranguelova, NLeSc
 % date created: 20 Oct 2016
-% last modification date: 
-% modification details: 
+% last modification date: 21 October 2016
+% modification details: added parameter extension
 %**************************************************************************
 % INPUTS:
 % data_path        where the dataset is located
+% [ext]            optional image filenames extension, {.png}
 %**************************************************************************
 % OUTPUTS:
 %**************************************************************************
@@ -18,12 +20,19 @@
 % EXAMPLES USAGE:
 % data_path = 'C:\Projects\eStep\LargeScaleImaging\Data\AffineRegions';
 % display_oxford_dataset_structured(data_path);
+%% OR
+% data_path = 'C:\Projects\eStep\LargeScaleImaging\Results\AffineRegions\';
+% ext = '_bin.png';
+% display_oxford_dataset_structured(data_path, ext);
 %**************************************************************************
 % REFERENCES: 
 %**************************************************************************
-function [] = display_oxford_dataset_structured(path)
+function [] = display_oxford_dataset_structured(path, ext)
 
 %% input control
+if nargin < 2
+    ext = '.png';
+end
 if nargin < 1
     error(' display_oxford_dataset_structured: the function expects minimum 1 input argument- the path of the dataset!');
 end
@@ -39,7 +48,7 @@ for i = 1: numel(test_cases)
     test_case = char(test_cases{i});
     test_path = fullfile(path,test_case);
     for j = 1:6
-        test_image = fullfile(test_path,[test_case num2str(j) '.png']);
+        test_image = fullfile(test_path,[test_case num2str(j) ext]);
         im = imread(test_image);
         figure(f); hold on;
         ind = (i-1)*6 + j;
