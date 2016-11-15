11106383de764b6fd91ca78db56b531e88cd2009
elboyran <e.ranguelova@esciencecenter.nl>
2016-11-15 14:44:58 +0100

    function to load and display same scene tests for the Oxford datatset

--- Software/MATLAB/Testing/comparision/contents.m
+++ Software/MATLAB/Testing/comparision/contents.m
@@ -14,3 +14,5 @@
 %                   images are of the same scene
 % test_IsSameScene_Oxford- testing IsSameScene function for comparision if 2
 %                   images are of the same scene for the Oxford dataset
+% load_saved_IsSameScene_Oxford- loading the results from testing if every
+%               pair of images are of the same scene for the Oxford dataset
---
+++ Software/MATLAB/Testing/comparision/load_saved_IsSameScene_Oxford.m
@@ -0,0 +1,67 @@
+% load_saved_IsSameScene_Oxford- loading the results from testing if every
+%               pair of images are of the same scene for the Oxford dataset
+%**************************************************************************
+% author: Elena Ranguelova, NLeSc
+% date created: 15-11-2016
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% last modification date: 
+% modification details: 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% NOTE:
+%**************************************************************************
+% REFERENCES
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% parameters
+% execution parameters
+verbose = true;
+visualize = true;
+visualize_matching_cost =  false;
+visualize_transf_similarity = false;
+visualize_dataset = false;
+sav_path = 'C:\Projects\eStep\LargeScaleImaging\Results\AffineRegions\Comparision\';
+sav_fname = [sav_path 'IsSameScene_Oxford_20161114_1752.mat'];
+
+% load the saved results
+load(sav_fname);
+
+% unpack parameters from  a structure
+v2struct(match_params);
+
+% paths
+data_path_or = 'C:\Projects\eStep\LargeScaleImaging\Data\AffineRegions\';
+ext = '.png';
+% data size
+data_size = 24;
+
+%% visualize the test dataset
+if visualize_dataset
+    if verbose
+        disp('Displaying the test dataset...');
+    end
+    display_oxford_dataset_structured(data_path_or);
+    pause(5);
+end
+
+%% visualize
+if visualize
+    gcmap = colormap(gray(256)); close;
+    hcmap = colormap(hot); close;
+    jcmap = colormap(jet); close;
+    f1 = format_figure(is_same_all, 6, gcmap, ...
+        [0 1], {'False','True'}, ...
+        'Is the same scene? All (structured) pairs of Oxford dataset.',...
+        YLabels);
+    if visualize_matching_cost 
+        f2 = format_figure(mean_costs, 6,jcmap, ...
+            [], [], ...
+            'Mean matching cost of all matches. All (structured) pairs of Oxford dataset.',...
+            YLabels);
+    end
+    if visualize_transf_similarity
+        f3 = format_figure(transf_sims, 6, hcmap, ...
+            [], [], ...
+            'Transformation between matches similarity (1- distance). All (structured) pairs of Oxford dataset.',...
+            YLabels);
+    end
+end
+
